43751293fb3fe71b0c155d2767b8e95f428a998d
Anwesh43 <anweshthecool0@gmail.com>
2018-07-26 10:36:18 +0530

    adding Animator claSS

--- LinkedAltStepStage.ts
+++ LinkedAltStepStage.ts
@@ -55,3 +55,26 @@ class State {
     }
 
 }
+
+class Animator {
+
+    animated : boolean = false
+
+    interval : number
+
+    start(cb : Function) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                cb()
+            })
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
