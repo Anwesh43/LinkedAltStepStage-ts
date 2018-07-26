7d7ccd4a88e641a29362e466f4f47a00770ee07c
Anwesh43 <anweshthecool0@gmail.com>
2018-07-26 10:45:18 +0530

    adding ASNode class

--- LinkedAltStepStage.ts
+++ LinkedAltStepStage.ts
@@ -78,3 +78,60 @@ class Animator {
         }
     }
 }
+
+class ASNode {
+
+    state : State = new State()
+
+    dir : number = 1
+
+    next : ASNode
+
+    prev : ASNode
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        const gap : number = w / NODES
+        const sc1 : number = Math.min(0.5, this.state.scale) * 2
+        const sc2 : number = Math.min(0.5, Math.max(0, this.state.scale - 0.5)) * 2
+        context.save()
+        context.translate(this.i * gap,(gap / 3) * (1 - (this.i % 2) * 2) * sc1)
+        context.beginPath()
+        context.moveTo(gap * sc2, 0)
+        context.lineTo(gap, 0)
+        context.stroke()
+        context.restore()
+    }
+
+    addNeighbor() {
+        if (this.i < NODES - 1) {
+            this.next = new ASNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    getNext(dir : number, cb : Function) : ASNode {
+        var curr : ASNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+
+
+}
